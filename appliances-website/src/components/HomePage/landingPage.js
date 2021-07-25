2dfba97dd49a78779c0975b51397407f43abfe2f
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-07-25 15:51:29 -0700

    got my first page right with image and text overlay

--- appliances-website/src/components/HomePage/landingPage.js
+++ appliances-website/src/components/HomePage/landingPage.js
@@ -4,8 +4,11 @@ import background from "../../images/landingPagePic.jpg";
 
 const LandingPage = () => {
     return (
-        <div style={{ backgroundImage: `url(${background})` }}>
-        Hello World
+        <div className="mainPic businessPageContainer" style={{ backgroundImage: `url(${background})` }}>
+              <nav className="navItems">
+              <a href="/storeInfo" alt="">Store Info</a>
+              </nav>
+        Mathias Appliances
       </div>
     );
   }
