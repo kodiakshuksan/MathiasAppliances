a76827fce277fa9fd094d6c6dc42c70dd6d22282
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-07-26 18:27:55 -0700

    rough draft complete

--- appliances-website/src/components/HomePage/landingPage.js
+++ appliances-website/src/components/HomePage/landingPage.js
@@ -4,11 +4,13 @@ import background from "../../images/landingPagePic.jpg";
 
 const LandingPage = () => {
     return (
-        <div className="mainPic businessPageContainer" style={{ backgroundImage: `url(${background})` }}>
-              <nav className="navItems">
-              <a href="/storeInfo" alt="">Store Info</a>
+        <div className="container-fluid">
+        <div className="businessPageContainer" style={{ backgroundImage: `url(${background})` }}>
+              <nav className="navItems" style={{ fontWeight: 'bolder', paddingTop: '10px'}}>
+              <a style={{ textDecoration: 'none', color: '#4E342E', fontSize: '25px', paddingLeft: '1050' }} href="/storeInfo" alt="Store Info">Store Info</a>
               </nav>
         <p className="businessName">Mathias Appliances</p>
+      </div>
       </div>
     );
   }
--- appliances-website/src/components/StoreInfoPage/storeInfo.js
+++ appliances-website/src/components/StoreInfoPage/storeInfo.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import "./storeInfo.css";
 import background from "../../images/storeInfoPic.jpg";
+import { Link, useLocation } from "react-router-dom";
+
+
 
 const StoreInfo = () => {
+
+    const location = useLocation();
+
     return (
-    <div className="infoPagePic mainPic" style={{ backgroundImage: `url(${background})` }}>
-    <nav className="navItems">
-    <a href="/landingPage" alt="">Home</a>
-    </nav>
-
-                <div className="location">Location</div>
-                    <div className="hours">Hours: By Appointment</div>
-                        <div className="phoneNumber">Phone Number:</div>
-                            <div className="inventory">Inventory-(OfferUp Link)</div>         
-            </div>
-
+        <div className="container-fluid"> 
+        <div className="storePageContainer" style={{ backgroundImage: `url(${background})` }}>
+               
+            <nav className="nav itemA" >
+            
+                <Link to="/" style={{ textDecoration: 'none', color: '#4E342E', fontWeight: 'bolder', paddingLeft: '1050px', fontSize: '25px'}} className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Home</Link>
+            
+                    <a style={{ textDecoration: 'none', color: '#4E342E', paddingTop: '7px', fontWeight: 'bolder', fontSize: '25px' }} href="https://offerup.com/p/21498274" alt="">Browse Inventory</a>
             
         
-
-    );
+            </nav>
+        
+                        <p className="address">Location: 15413 1st Ave Ct South Blg. G Tacoma, WA 98444</p>
+                            <p className="hours">Showroom Hours: By Appointment</p>
+                                <p className="phoneNumber">Phone Number:1-443-949-2766</p>
+                    </div>
+                    </div>
+            
+         );
 }
 
 
